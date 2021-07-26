194107a9fd328d9add3f4692a0b8141e2564dcd1
Mohshin <ms@tectusdreamlab.com>
2021-07-26 23:10:55 +0800

    feat: Load avtar images using SDWebImage

--- ContactApp/Modules/ContactList/ContactTableViewCell.swift
+++ ContactApp/Modules/ContactList/ContactTableViewCell.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 class ContactTableViewCell: UITableViewCell {
     
@@ -14,6 +15,13 @@ class ContactTableViewCell: UITableViewCell {
     @IBOutlet weak var fullNameLabel: UILabel!
     
     func configure(using viewModel: ContactCellViewModel) {
+        avtarImageView.sd_setImage(
+            with: viewModel.avatarURL,
+            placeholderImage: nil,
+            options: [],
+            context: [.imageThumbnailPixelSize : CGSize(width: 200, height: 200)]
+        )
+        avtarImageView.sd_imageTransition = .fade(duration: 0.3)
         fullNameLabel.text = viewModel.fullName
     }
 }
