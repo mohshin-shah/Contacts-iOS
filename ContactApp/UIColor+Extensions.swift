cd3d7d48e51d291229c496cc6a1fee92f4794007
Mohshin <ms@tectusdreamlab.com>
2021-07-26 21:38:47 +0800

    feat: Add color extension to manage theme colors at one place

---
+++ ContactApp/UIColor+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extensions.swift
+//  ContactApp
+//
+//  Created by Mohshin on 26/7/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var primary: UIColor {
+        UIColor(red: 130/255, green: 219/255, blue: 195/255, alpha: 1.0)
+    }
+}
