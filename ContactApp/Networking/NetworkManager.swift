1acd8d16564c7e9e01b79c9e5adf4d7aeaf37b8d
Mohshin <ms@tectusdreamlab.com>
2021-07-26 17:23:23 +0800

    feat: Add api request call using AF and Promises

--- ContactApp/Networking/NetworkManager.swift
+++ ContactApp/Networking/NetworkManager.swift
@@ -5,7 +5,9 @@
 //  Created by Mohshin on 26/7/21.
 //
 
+import Alamofire
 import Foundation
+import Promises
 
 final class NetworkManager {
     public static var shared: NetworkManager {
@@ -21,6 +23,7 @@ final class NetworkManager {
     private static var _shared: NetworkManager?
     
     let defaultBaseURL: URL
+    let apiSession = Session.default
     
     init(baseURL: URL) {
         self.defaultBaseURL = baseURL
@@ -29,5 +32,33 @@ final class NetworkManager {
     
     private func configureWebServiceEndPoints() {
         UserWebServiceEndpoint.baseURL = defaultBaseURL
+    }
+}
+
+// MARK: - Requests
+extension NetworkManager {
+    /// Returns a promise that requests `Decodable` value at the specified API endpoint
+    ///
+    /// - Parameters:
+    ///   - endpoint: The endpoint for the request.
+    ///   - decoder: A JSONDecoder to decode the response data into value.
+    func requestDecodable<T>(_ endpoint: Endpoint, decoder: JSONDecoder = JSONDecoder()) -> Promise<T> where T: Decodable {
+        Promise(on: .global()) { fulfill, reject in
+            let request = self.apiSession.request(endpoint)
+            request.validate().responseDecodable(decoder: decoder) { (response: AFDataResponse<T>) in
+                response.resolve(fulfill, reject)
+            }
+        }
+    }
+}
+
+private extension DataResponse {
+    func resolve(_ fulfill: (Success) -> Void, _ reject: (Error) -> Void) {
+        switch result {
+        case let .success(value):
+            fulfill(value)
+        case let .failure(error):
+            reject(error)
+        }
     }
 }
