d1d2d4750dfa1d1d13d80c9fb52045201e17d79e
Mohshin <ms@tectusdreamlab.com>
2021-07-26 12:21:13 +0800

    Initial Setup

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Mohshin on 26/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
